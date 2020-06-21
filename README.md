863158623b0e04f2d0a8a50f2d63b8264b640bef
Yashraj Mishra <mishra.yashrajs@outlook.com>
2020-06-21 23:46:28 +0530

    express added

---
+++ README.md
@@ -0,0 +1,14 @@
+# Under Development
+
+## prerequisites
+
+- nodemon
+- live-server
+
+## tasks
+
+- [ ] get list of all files on folder
+- [ ] make a responsive web page with data
+- [ ] bundle app to .exe
+- [ ] show all movies poster
+- [ ] add aditional info of movies
--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-console.log("Hello")
+const fs = require('fs');
+const _ = require('lodash');
+const express = require('express');
+const app = express();
+
+console.clear();
+
+let fileList = [];
+
+fs.readdir('sample', (err, data) => {
+	if (err) {
+		console.log(err);
+	} else {
+		console.log(data);
+		fileList = data;
+		app.get('/', function (req, res) {
+			res.send(`Hello World yo ${data.toString()}`);
+		});
+		app.listen(3000);
+	}
+});
+
+// code to create sample
+
+// for (let i = 0; i < 10; i++) {
+//   fs.writeFile(`./sample/file${i}.txt`, `This is sample${i}`, function (
+//     err
+//   ) {
+//     if (err) throw err;
+//     console.log("Results Received");
+//   });
+// }
+
+// fs.readFile("sample/DATA", "utf8", (err, contents) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(contents);
+//   }
+// });
