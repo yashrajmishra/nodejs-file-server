6452da086bca0f616c5607980634c7f70d301844
Yashraj Mishra <mishra.yashrajs@outlook.com>
2020-06-22 22:57:27 +0530

    3rd day #100daysofCode

--- README.md
+++ README.md
@@ -7,8 +7,8 @@
 
 ## tasks
 
-- [ ] get list of all files on folder
-- [ ] make a responsive web page with data
+- [x] get list of all files on folder
+- [x] make a responsive web page with data
 - [ ] bundle app to .exe
 - [ ] show all movies poster
 - [ ] add aditional info of movies
--- index.js
+++ index.js
@@ -2,39 +2,17 @@ const fs = require('fs');
 const _ = require('lodash');
 const express = require('express');
 const app = express();
-
 console.clear();
-
-let fileList = [];
 
 fs.readdir('sample', (err, data) => {
 	if (err) {
 		console.log(err);
 	} else {
 		console.log(data);
-		fileList = data;
-		app.get('/', function (req, res) {
-			res.send(`Hello World yo ${data.toString()}`);
+		app.set('view engine', 'ejs');
+		app.get('/', (req, res) => {
+			res.render('index', { data });
 		});
 		app.listen(3000);
 	}
 });
-
-// code to create sample
-
-// for (let i = 0; i < 10; i++) {
-//   fs.writeFile(`./sample/file${i}.txt`, `This is sample${i}`, function (
-//     err
-//   ) {
-//     if (err) throw err;
-//     console.log("Results Received");
-//   });
-// }
-
-// fs.readFile("sample/DATA", "utf8", (err, contents) => {
-//   if (err) {
-//     console.log(err);
-//   } else {
-//     console.log(contents);
-//   }
-// });
